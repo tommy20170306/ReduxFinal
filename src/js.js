3dcbf76b442d227ebff06e1af10af106e824b9d2
developer <developer@local.dev>
2017-07-27 17:41:46 +0800

    finish post view

---
+++ src/js.js
@@ -0,0 +1,21 @@
+// 1. adjacent parentheses
+
+(true, (num)=>{return num*2})(5,10)
+// -> num = first param = 5 ->
+return 5*2
+
+// 2. Windows button not working
+// Windows Powershell: run sfc /scannow
+
+// 3. in vs hasOwnProperty
+
+ if (key in object) // include protytype chain
+
+ if (object.hasOwnProperty(key)) // only its own property
+
+ // 4. JS <=> JQ
+$('#elm').css('color') === (window.getComputedStyle(elm))['color']
+
+// 5. array
+let a = [1,2,3];
+console.log(...a); // 1 2 3
